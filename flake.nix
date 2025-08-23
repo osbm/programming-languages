4ef4236aad192f2e3a4d71c6a50f7d7db81f9987
osbm <osbm@osbm.dev>
2025-08-23 13:47:04 +0300

    init flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A flake that contains some programming language examples";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
