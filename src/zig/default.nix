60a53d52deb1c5422174a24337616b58afc2d52f
osbm <osbm@osbm.dev>
2025-08-24 01:40:47 +0300

    compile instead

--- src/zig/default.nix
+++ src/zig/default.nix
@@ -1,6 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.writeShellScriptBin "zig" ''
-  cd ${./.}
-  ${pkgs.zig}/bin/zig run main.zig
-''
+pkgs.stdenv.mkDerivation {
+  name = "zig";
+  src = ./main.zig;
+  unpackPhase = "true";
+  buildInputs = [ pkgs.zig ];
+  buildPhase = ''
+    cp $src main.zig
+    export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+    mkdir -p $ZIG_GLOBAL_CACHE_DIR
+    zig build-exe main.zig -OReleaseFast --cache-dir $TMPDIR/zig-cache
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp main $out/bin/main
+  '';
+  meta.mainProgram = "main";
+}
