6376a67c24d59944d6241615a4fdcfeddf3ac07c
osbm <osbm@osbm.dev>
2025-08-23 16:10:46 +0300

    cleaner

--- src/python/default.nix
+++ src/python/default.nix
@@ -1,7 +1,2 @@
-{pkgs, lib, ...}:
-let 
-  script = pkgs.writers.makeScriptWriter {
-    interpreter = lib.getExe pkgs.python3;
-  } "python" (builtins.readFile ./main.py);
-in
-  pkgs.writeScriptBin "python" (builtins.readFile script)
+{pkgs, ...}:
+pkgs.writers.writePython3Bin "python" {} (builtins.readFile ./main.py)
