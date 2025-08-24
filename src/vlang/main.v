c967ffeed6fb2ac1ab559c7c2e19593575aac844
osbm <osbm@osbm.dev>
2025-08-25 00:59:23 +0300

    fix vlang

--- src/vlang/main.v
+++ src/vlang/main.v
@@ -1,11 +1,11 @@
-fn collatz_len_and_peak(x int) (int, int) {
-	mut steps := 0
+fn collatz_len_and_peak(x i64) (i64, i64) {
+	mut steps := i64(0)
 	mut peak := x
 	mut n := x
 
 	for n != 1 {
-		if n & 1 == 1 { // odd
-			n = 3 * n + 1
+		if n & i64(1) == i64(1) { // odd
+			n = i64(3) * n + i64(1)
 		} else { // even
 			n = n >> 1
 		}
@@ -18,13 +18,13 @@ fn collatz_len_and_peak(x int) (int, int) {
 	return steps, peak
 }
 
-fn scan_upto(limit int) (int, int, int, int) {
-	mut best_n := 1
-	mut best_steps := 0
-	mut best_peak := 1
-	mut xor_steps := 0
+fn scan_upto(limit i64) (i64, i64, i64, i64) {
+	mut best_n := i64(1)
+	mut best_steps := i64(0)
+	mut best_peak := i64(1)
+	mut xor_steps := i64(0)
 
-	for n in 1..limit + 1 {
+	for n in i64(1)..limit + i64(1) {
 		steps, peak := collatz_len_and_peak(n)
 		xor_steps ^= steps
 		if steps > best_steps {
@@ -40,7 +40,7 @@ fn scan_upto(limit int) (int, int, int, int) {
 fn main() {
 	println('hello world!')
 
-	n_limit := 100000
+	n_limit := i64(1000000)
 	nstar, steps, peak, checksum := scan_upto(n_limit)
 	println('collatz_longest(1..$n_limit)')
 	println('n*=$nstar')
