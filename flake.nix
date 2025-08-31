cd7669ab43631b5000896a12f5a0815c5e21db38
osbm <osbm@osbm.dev>
2025-08-31 17:51:27 +0300

    add docker images

--- flake.nix
+++ flake.nix
@@ -58,10 +58,33 @@
       in
         # Filter out override functions that confuse nix flake show
         # and filter out packages that don't support the current system
-        pkgs.lib.filterAttrs (name: value: 
-          pkgs.lib.isDerivation value && 
+        pkgs.lib.filterAttrs (name: value:
+          pkgs.lib.isDerivation value &&
           builtins.elem system (value.meta.platforms or supportedSystems)
         ) allPackages
+    );
+    dockerImages = forAllSystems (
+      system: let
+        pkgs = makePkgs system;
+        allPackages = pkgs.callPackage ./src {};
+        compatiblePackages = pkgs.lib.filterAttrs (name: value:
+          pkgs.lib.isDerivation value &&
+          builtins.elem system (value.meta.platforms or supportedSystems)
+        ) allPackages;
+      in
+        pkgs.lib.mapAttrs (name: package:
+          pkgs.dockerTools.buildImage {
+            name = "programming-languages-${name}";
+            tag = "latest";
+            config = {
+              Cmd = [ "${package}/bin/${package.meta.mainProgram or name}" ];
+              WorkingDir = "/";
+              Env = [
+                "PATH=${pkgs.coreutils}/bin:${pkgs.bash}/bin"
+              ];
+            };
+          }
+        ) compatiblePackages
     );
   };
 }
