8a0357b0a72ad21ef98e8a46c93e8012b504ab51
osbm <osbm@osbm.dev>
2025-09-01 08:52:15 +0300

    add nixpkgs to nix

--- src/nix/default.nix
+++ src/nix/default.nix
@@ -1,4 +1,12 @@
 {pkgs, ...}:
+let
+  nixpkgs = pkgs.fetchFromGitHub {
+    owner = "nixos";
+    repo = "nixpkgs";
+    rev = "nixpkgs-unstable";
+    hash = "sha256-mBecwgUTWRgClJYqcF+y4O1bY8PQHqeDpB+zsAn+/zA=";
+  };
+in
 pkgs.writeShellScriptBin "nix" ''
-  ${pkgs.nix}/bin/nix eval --raw --option max-call-depth 1000000 -f ${./main.nix}
+  ${pkgs.nix}/bin/nix eval --raw -I nixpkgs=${nixpkgs} --option max-call-depth 1000000 -f ${./main.nix}
 ''
