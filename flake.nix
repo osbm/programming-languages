068453c7802e3013ea847db1502145ee12b4942f
osbm <osbm@osbm.dev>
2025-09-02 15:20:53 +0300

    add cuda to nixpkgs

--- flake.nix
+++ flake.nix
@@ -19,36 +19,6 @@
     makePkgs = system:
       import nixpkgs {
         inherit system;
-        config = {
-          cudaSupport = true;
-          allowUnfreePredicate = pkg:
-            builtins.elem (nixpkgs.lib.getName pkg) [
-              "cuda-merged"
-              "cuda_cuobjdump"
-              "cuda_gdb"
-              "cuda_nvcc"
-              "cuda_nvdisasm"
-              "cuda_profiler"
-              "cuda_nvprune"
-              "cuda_cccl"
-              "cuda_cudart"
-              "cuda_cupti"
-              "cuda_cuxxfilt"
-              "cuda_nvml_dev"
-              "cuda_nvrtc"
-              "cuda_nvtx"
-              "cuda_profiler_api"
-              "cuda_sanitizer_api"
-              "libcublas"
-              "libcurand"
-              "libcufft"
-              "libcusolver"
-              "libnvjitlink"
-              "libcusparse"
-              "libnpp"
-              "nvidia-x11"
-            ];
-        };
       };
   in {
     packages = forAllSystems (
